5a5563dceafdd4e4027a2863acf74b64f64fcd85
Gyeongtae Min <prierkt@gmail.com>
2022-06-19 23:21:08 +0900

    add views

--- django_prac/main/polls/views.py
+++ django_prac/main/polls/views.py
@@ -1,4 +1,21 @@
-from django.http import HttpResponse
+from django.http import  Http404
+from django.shortcuts import render, get_object_or_404
+
+from polls.models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    context = {'lastest_question_list': lastest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def result(request, question_id):
+    response = "You're looking at the result of question %s."
+    return render(request, 'polls/result.html', response)
+
+def vote(request, question_id):
+    return render(request, 'polls/vote.html', question_id)
