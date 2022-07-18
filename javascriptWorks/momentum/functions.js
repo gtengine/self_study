525fc01b2359b15162c66a73f7debd3ed15c1508
Gyeongtae Min <prierkt@gmail.com>
2022-07-18 19:07:11 +0900

    recap

--- javascriptWorks/momentum/functions.js
+++ javascriptWorks/momentum/functions.js
@@ -28,3 +28,29 @@ const player = {
 };
 console.log(player.name);
 player.sayBye("karina");
+
+
+
+
+const calculator = {
+    plus: function(a, b){
+        console.log(a + b);
+    },
+    minus: function(a, b){
+        console.log(a - b);
+    },
+    times: function(a, b){
+        console.log(a * b);
+    },
+    divide: function(a, b){
+        console.log(a / b);
+    },
+    power: function(a, b){
+        console.log(a ** b);
+    }
+};
+calculator.plus(8, 2);
+calculator.minus(8, 2);
+calculator.times(8, 2);
+calculator.divide(8, 2);
+calculator.power(8, 2);
