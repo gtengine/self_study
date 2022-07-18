3712193dd81cc38cbc6cefd82a92daf3542d0e67
Gyeongtae Min <prierkt@gmail.com>
2022-07-18 14:23:04 +0900

    data type array

---
+++ javascriptWorks/momentum/arrays.js
@@ -0,0 +1,20 @@
+// array
+
+const mon = 'mon';
+const tue = 'tue';
+const wed = 'wed';
+const thu = 'thu';
+const fri = 'fri';
+const sat = 'sat';
+const sun = 'sun';
+const dayOfWeek1 = [mon, tue, wed, thu, fri, sat, sun];
+const nonsense = [1, 2, 'hello', false, null, true, undefined];
+console.log(dayOfWeek1, nonsense);
+
+const dayOfWeek2 = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+console.log(dayOfWeek2);
+console.log(dayOfWeek2[5]); // sat
+console.log(dayOfWeek2);
+
+dayOfWeek2.push('sun')
+console.log(dayOfWeek2)
