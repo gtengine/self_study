d0a0ba62e08c86dd59f1304fc20f5e85663600d0
Gyeongtae Min <prierkt@gmail.com>
2022-07-18 19:38:57 +0900

    what is return, how to use return

--- javascriptWorks/momentum/functions.js
+++ javascriptWorks/momentum/functions.js
@@ -32,7 +32,7 @@ player.sayBye("karina");
 
 
 
-const calculator = {
+const calculator1 = {
     plus: function(a, b){
         console.log(a + b);
     },
@@ -49,8 +49,48 @@ const calculator = {
         console.log(a ** b);
     }
 };
-calculator.plus(8, 2);
-calculator.minus(8, 2);
-calculator.times(8, 2);
-calculator.divide(8, 2);
-calculator.power(8, 2);
+calculator1.plus(8, 2);
+calculator1.minus(8, 2);
+calculator1.times(8, 2);
+calculator1.divide(8, 2);
+calculator1.power(8, 2);
+
+
+
+// return
+function calculateKrAge(ageOfForeigner){
+    return ageOfForeigner + 2; // return이 수행되고 function은 종료된다.
+}
+const age = 30;
+const krAge = calculateKrAge(age);
+console.log(krAge);
+
+
+
+const calculator2 = {
+    plus: function(a, b){
+        return a + b;
+    },
+    minus: function(a, b){
+        return a - b;
+    },
+    times: function(a, b){
+        return a * b;
+    },
+    divide: function(a, b){
+        return a / b;
+    },
+    power: function(a, b){
+        return a ** b;
+    }
+};
+const plusResult = calculator2.plus(8, 2);
+console.log(plusResult);
+const minusResult = calculator2.minus(plusResult, 2);
+console.log(minusResult);
+const timesResult = calculator2.times(8, minusResult);
+console.log(timesResult);
+const divideResult = calculator2.divide(timesResult, plusResult);
+console.log(divideResult);
+const powerResult = calculator2.power(divideResult, 2);
+console.log(powerResult);
