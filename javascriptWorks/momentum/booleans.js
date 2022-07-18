d83f8510b3abf6ac8930943a8f7f9ba85819c9c5
Gyeongtae Min <prierkt@gmail.com>
2022-07-18 14:22:42 +0900

    data type boolean

---
+++ javascriptWorks/momentum/booleans.js
@@ -0,0 +1,15 @@
+// boolean
+
+const amIFat = true;
+console.log(amIFat);
+
+const amIThin = false;
+console.log(amIFat);
+
+// null : 아무것도 정의되지 않음.
+const myWeights = null;
+console.log(myWeights);
+
+// undefind : 변수는 존재하지만 값이 없음.
+let something;
+console.log(something, myWeights);
