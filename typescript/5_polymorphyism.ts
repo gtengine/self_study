c7112793fa0ee64c44c4ca53fe532e2a9cc52600
Gyeongtae Min <prierkt@gmail.com>
2022-07-31 17:26:03 +0900

    create local api with polymorphyism

---
+++ typescript/5_polymorphyism.ts
@@ -0,0 +1,27 @@
+interface SStorage<T> {
+  [key: string]: T;
+}
+
+class LocalStorage<T> {
+  private storage: SStorage<T> = {};
+  set(key: string, value: T) {
+    this.storage[key] = value;
+  }
+  remove(key: string) {
+    delete this.storage[key];
+  }
+  get(key: string): T {
+    return this.storage[key];
+  }
+  clear() {
+    this.storage = {};
+  }
+}
+
+const stringStorage = new LocalStorage<string>();
+stringStorage.get("get");
+stringStorage.set("hello", "how are you");
+
+const booleanStorage = new LocalStorage<boolean>();
+booleanStorage.get("bool");
+booleanStorage.set("is man", true);
