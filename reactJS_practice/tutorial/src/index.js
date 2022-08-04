19effe068700ab677cb5e20370e0ad03df7fbca6
Gyeongtae Min <prierkt@gmail.com>
2022-08-04 21:11:06 +0900

    create a react project and how to import css files

---
+++ reactJS_practice/tutorial/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// css파일을 import 하여 적용시킨다.
+// import "./style.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
