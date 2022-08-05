57dd243f67d1a872a7b71683c7cdf8cedd2d1b58
Gyeongtae Min <prierkt@gmail.com>
2022-08-05 14:21:29 +0900

    what is useEffect and how to use it

--- reactJS_practice/tutorial/src/App.js
+++ reactJS_practice/tutorial/src/App.js
@@ -1,11 +1,30 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => setKeyword(event.target.value);
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  // useEffect: 무슨 일이 일어나든 코드가 단 한번만 실행되도록 하거나 어떤 조건에 코드가 실행되도록 조정할 수 있다.
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!!!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me!</button>
     </div>
   );
 }
