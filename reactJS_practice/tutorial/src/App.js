15a0c94e35a3d8a7494067d7a396907bbff83d2c
Gyeongtae Min <prierkt@gmail.com>
2022-08-07 17:14:51 +0900

    create to do list

--- reactJS_practice/tutorial/src/App.js
+++ reactJS_practice/tutorial/src/App.js
@@ -1,29 +1,37 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("created ^^");
-    return () => console.log("destroyed ㅜㅜ");
-  }, []);
-
-  // function byeFn() {
-  //   console.log("destroyed ㅜㅜ");
-  // }
-  // function hiFn() {
-  //   console.log("created ^^");
-  //   return byeFn;
-  // }
-  // useEffect(hiFn, []);
-  // return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  // state 값을 수정할 때에는 절대 직접 변경하지 않고, modifier를 통해 수정한다.
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    // array의 원소들을 새로운 array로 가져올 때는 ...을 array앞에 입력한다.
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
