5e4a162824289b6e1bced2248749912a2cf74bc6
Gyeongtae Min <prierkt@gmail.com>
2022-08-16 14:12:34 +0900

    Painting lines

--- javascriptWorks/meme-maker/app.js
+++ javascriptWorks/meme-maker/app.js
@@ -2,16 +2,27 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
 
-ctx.fillRect(210 - 33, 200 - 20, 15, 100);
-ctx.fillRect(350 - 33, 200 - 20, 15, 100);
-ctx.fillRect(255 - 33, 200 - 20, 60, 200);
+const colors = [
+  "#ff3838",
+  "#ffb8b8",
+  "#c56cf0",
+  "#ff9f1a",
+  "#fff200",
+  "#32ff7e",
+  "#7efff5",
+  "#ffeaa7",
+  "#d63031",
+];
 
-ctx.arc(250, 100, 50, 0, 2 * Math.PI);
-ctx.fill();
+function onClick(event) {
+  ctx.beginPath();
+  ctx.moveTo(400, 400);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
 
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(270, 90, 8, Math.PI, 2 * Math.PI);
-ctx.arc(230, 90, 8, Math.PI, 2 * Math.PI);
-ctx.fill();
+canvas.addEventListener("mousemove", onClick);
